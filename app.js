4660834ea16da453b438db41a2c5bf5f784dd9a6
aidan_jabbarzadeh <jabbarz2@illinois.edu>
2020-06-25 15:33:05 -0500

    trying to get wiki post route working

--- app.js
+++ app.js
@@ -2,32 +2,37 @@ const express = require("express");
 const morgan = require("morgan");
 const Sequelize = require('sequelize');
 const wikiRouter = require('./routes/wiki');
-const userRouter = require('./routes/user');
+// const userRouter = require('./routes/user');
+const app = express();
 
 const getLayout = require("./views/layout");
 
-
-
-// Where your server and express app are being defined:
-const models = require('./models/index.js');
-models.db.sync({ force: true }) // this drops all tables then recreates them based on our JS definitions
-const { Page, User } = models;
 const db = new Sequelize('postgres://localhost:5432/wikistack', {
   logging: false
 });
 
+// Where your server and express app are being defined:
+const models = require('./models');
+models.db.sync({ force: true }) // this drops all tables then recreates them based on our JS definitions
+// const { Page, User } = models;
 
-
-const app = express();
 app.use('/wiki', wikiRouter);
+// app.use('/user', userRouter);
+app.get("/", (req, res) => {
+    res.redirect("/wiki");
+});
+
+// parses url-encoded bodies
+app.use(express.urlencoded({ extended: false }));
+
+// parses json bodies
+app.use(express.json())
+  
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
-const { addPage, editPage, main, userList, userPages, wikiPage } = require("./views/index.js");
-
-
+// const { addPage, editPage, main, userList, userPages, wikiPage } = require("./views/index.js");
 
 const PORT = 3000;
-
 
 app.get("/", (req, res) => {
   res.send(getLayout("CONTENT HERE"));
@@ -40,14 +45,28 @@ db.authenticate().
   })
 
 const init = async () => {
-  await models.User.sync();
-  await models.Page.sync();
-  await models.db.sync();
+    try {
+        /* console.log("ENTERED INIT");
+
+        await models.User.sync();
+        console.log("SYNCED USERS");
+
+        await models.Page.sync();
+        console.log("SYNCED PAGE"); */ 
+        await models.db.sync({force:true});
+        app.listen(PORT, () => { // app is name of our app
+            console.log(`Server is listening on port ${PORT}!`);
+          });
+    } catch(err) {
+        // console.log(err);
+    }
+
+
+    // await models.db.sync();
+    // console.log("SYNCED DB");
 
   // make sure that you have a PORT constant and to replace the name below with your express app
-  app.listen(PORT, () => { // app is name of our app
-    console.log(`Server is listening on port ${PORT}!`);
-  });
+  
 }
 
 init();
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { addPage } = require("../views");
-// const getLayout = require("./views/layout");
+const addPage  = require("../views").addPage;
+const getLayout = require("../views/layout");
 
 router.get('/', (req, res, next) => {
-  res.send('got to GET /wiki/');
-  // res.send(getLayout("CONTENT HERE"));
+  res.send(getLayout("CONTENT HERE"));
   // console.log("hello world");
 });
 
-router.post('/', (req, res, next) => {
-  res.send('got to POST /wiki/');
+router.post('/', async (req, res, next) => {
+  try {
+    res.json(req.body);
+    res.send('got to POST /wiki/');
+  } catch (err) {
+    console.log(err);
+  }
+  
 });
 
-router.get('/add', (req, res, next) => {
-  res.send('got to GET /wiki/add');
+router.get('/add', async (req, res, next) => {
+  res.send(addPage());
 });
-
-// router.get("/add", (req, res, next) => {
-// res.send(addPage());
-// });
-
 
 module.exports = router;
